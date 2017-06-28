10c4e6582bb7c6284e4e641e9aa75460f7cce2cc
patrickcarmo <patrick.a.carmo@gmail.com>
2017-06-27 23:33:08 -0300

    Authentication, Search and Chat application with Firebase

--- ConatusChat/src/app/app.firebase.config.ts
+++ ConatusChat/src/app/app.firebase.config.ts
@@ -1,8 +1,8 @@
   export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyC4vIHXg39j5pkNfNQoAwLF9zK-8ol7WLA",
-    authDomain: "conatuschat.firebaseapp.com",
-    databaseURL: "https://conatuschat.firebaseio.com",
-    projectId: "conatuschat",
-    storageBucket: "conatuschat.appspot.com",
-    messagingSenderId: "924392324106"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   }
